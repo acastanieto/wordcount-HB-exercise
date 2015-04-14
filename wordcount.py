7decf0dc7f2439ec8195704134330987b52f21fa
Hackbright Student <info@hackbrightacademy.com>
2015-04-13 19:35:46 -0700

    Modified print function to sort by counter before print words and counts

--- wordcount.py
+++ wordcount.py
@@ -11,22 +11,38 @@ from collections import Counter
 script, filename = argv
 
 def count_words(filename):
+    """Creates a Counter collection of the words occuring in a text file."""
+
     text_file = open(filename)
     text = text_file.read()
     text = text.rstrip().lower().replace("\n", " ")
+   
     no_punct = ""
+    
     for char in text:
         if char.isalpha() or char == " ":
             no_punct += char
+   
     word_list = no_punct.split(" ")
-
     word_counts = Counter(word_list)
 
     return word_counts
 
 
-def print_wordcount(word_counts):
-    for word, count in word_counts.iteritems():
+def sort_and_print(word_counts):
+    sorted_tuples_by_count = word_counts.most_common() # one way
+    # another way:
+    # sorted_tuples_by_count = sorted(word_counts.items(), key=lambda pair: pair[1], reverse=True)
+    for word, count in sorted_tuples_by_count:
         print word, count
 
-print_wordcount(count_words(filename))
+    # sorted_wordcounts = sorted(word_counts.items(), key=lambda pair:pair[1], reverse=True)
+    # for word, count in sorted_wordcounts:
+    #     print word, count
+    # for word, count in word_counts.iteritems():
+    #     print word, count
+
+
+sort_and_print(count_words(filename))
+
+# http://stackoverflow.com/questions/20950650/how-to-sort-counter-by-value-python
